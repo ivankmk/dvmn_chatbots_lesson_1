c44bc8bb73f008648947e84d8277adc196a944a4
IvanKMK <kumeyko@hotmail.com>
2021-01-31 17:15:44 +0100

    reshaped the __main__

--- main.py
+++ main.py
@@ -6,15 +6,8 @@ import os
 import time
 
 
-def send_message(token, chat_id, text):
-    bot = telegram.Bot(token=token)
-    bot.send_message(chat_id=chat_id, text=text)
-
-
-def send_tg_notification(url):
-    logging.info('Bot is running.')
-    tg_token = os.environ.get('TG_TOKEN')
-    tg_chat_id = os.environ.get('TG_CHAT_ID')
+def send_tg_notification(url, chat_id):
+    logger.info('Bot is running.')
     token = {'Authorization': os.environ.get('DVMN_TOKEN')}
     params = ''
     while True:
@@ -36,25 +29,45 @@ def send_tg_notification(url):
                 task_name = lesson_details['lesson_title']
                 is_negative = lesson_details['is_negative']
                 if is_negative:
-                    send_message(tg_token,
-                                 tg_chat_id,
-                                 f"У вас проверили работу '{task_name}'. \
-                                  К сожалению нашлись ошибки :-(")
+                    bot.send_message(
+                        tg_chat_id,
+                        f"У вас проверили работу '{task_name}'. \
+                        К сожалению нашлись ошибки :-(")
                 else:
-                    send_message(tg_token,
-                                 tg_chat_id,
-                                 f"У вас проверили работу '{task_name}'. \
-                                Все ок - можно приступать к следующему :-)")
+                    bot.send_message(
+                        tg_chat_id,
+                        f"У вас проверили работу '{task_name}'. \
+                        Все ок - можно приступать к следующему :-)")
 
-        except requests.exceptions.ReadTimeout:
+        except requests.exceptions.ReadTimeout as e:
+            logger.error(e)
             continue
         except requests.exceptions.ConnectionError:
             time.sleep(90)
             continue
+        except Exception as e:
+            logger.error(e)
 
 
 if __name__ == "__main__":
-    logging.getLogger().setLevel(logging.INFO)
+
     load_dotenv()
+    tg_token = os.environ.get('TG_TOKEN')
+    tg_chat_id = os.environ.get('TG_CHAT_ID')
+    bot = telegram.Bot(token=tg_token)
+    bot_logger = telegram.Bot(token=tg_token)
+
     url = 'https://dvmn.org/api/long_polling/'
-    send_tg_notification(url)
+
+    class MyLogsHandler(logging.Handler):
+
+        def emit(self, record):
+            log_entry = self.format(record)
+            bot_logger.send_message(chat_id=tg_chat_id, text=log_entry)
+
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+    logger.addHandler(MyLogsHandler())
+
+    send_tg_notification(url, tg_chat_id)
+
